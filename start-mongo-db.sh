b051174157d69da8a003c52a3e10f36c3d947480
tenfromfive <46288978+tenfromfive@users.noreply.github.com>
2019-04-11 09:45:55 -1000

    Update start-mongo-db.sh
    added grep to cat

--- start-mongo-db.sh
+++ start-mongo-db.sh
@@ -4,4 +4,4 @@
 sudo service mongod start
 
 #verify "[initandlisten] waiting for connections on port 27017" is in this log file
-sudo cat /var/log/mongodb/mongod.log
+sudo cat /var/log/mongodb/mongod.log | grep 27017
